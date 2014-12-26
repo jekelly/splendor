bef35912dd7bfba90df8eac5e6aa009da3741d2e
jekelly <jinxremoving@gmail.com>
2014-12-25 17:06:20 -0800

    Add Noble images and some minor UI tweaks.

---
+++ Splendor/ViewModel/Converters/NobleToImageSourceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Splendor.Model;
+using Windows.UI.Xaml.Data;
+
+namespace Splendor.ViewModel
+{
+	public class NobleToImageSourceConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			Noble noble = (Noble)value;
+			return string.Format("ms-appx:///Assets/Themes/Space/Nobles/noble{0}.jpg", noble.id);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
