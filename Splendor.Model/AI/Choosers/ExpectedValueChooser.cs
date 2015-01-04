d4d1b1a4dc9849b65155ae43ed8d43a68fa5f71d
Jeff Kelly <jinxremoving@gmail.com>
2015-01-04 13:27:44 -0800

    Add an abstract chooser that evaluates all possible moves against an eval function, and a basic implementation.

---
+++ Splendor.Model/AI/Choosers/ExpectedValueChooser.cs
@@ -0,0 +1,152 @@
+﻿namespace Splendor.Model.AI
+{
+	using System;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	public class ExpectedValueChooser : GreedyChooser
+	{
+		// Expected value of a position can be estimated via:
+		// current score (known)
+		// for nobles: value of noble * distance to noble * prob of getting that noble
+		// for cards in market: value of card * prob of getting that card * distance to card
+		// for cards in hand: value of card * distance to card
+		// for cards in neither: value of card * distance to card * prob of card coming out
+
+		public ExpectedValueChooser(int playerIndex)
+			: base(playerIndex)
+		{
+		}
+
+		protected async override Task<double> EvaluateStateAsync(IGame state)
+		{
+			return await Task.Run(() =>
+				{
+					double value = 0;
+					IPlayer currentPlayer = state.GetPlayer(this.PlayerIndex);
+
+					double[] gemValues = CalculateGemValues(state);
+
+					value += currentPlayer.Score;
+					for (int n = 0; n < state.Nobles.Length; n++)
+					{
+						Noble noble = state.Nobles[n];
+						value += 3.0 * NobleRatio(state, currentPlayer, noble);
+					}
+					for (int m = 0; m < state.Market.Length; m++)
+					{
+						Card card = state.Market[m];
+						if (card.id == Rules.SentinelCard.id) continue;
+						value += (card.value + gemValues[card.gives]) * CardRatio(state, currentPlayer, card);
+						// todo: secondary value of card as a gem in terms of expectations of its future value?
+					}
+					//foreach (Card card in currentPlayer.Hand)
+					//{
+					//	if (card.id == Rules.SentinelCard.id) continue;
+					//	int totalCost = card.costBlack + card.costBlue + card.costGreen + card.costRed + card.costWhite;
+					//	value += card.value * (1.0 - (Distance(card, currentPlayer) / totalCost));
+					//}
+
+					return value;
+				});
+		}
+
+		// card value: value + (# cards in market it contributes to * card value * contribution) + (nobles * noble value * contribution)
+
+		private static int TotalCost(Card card)
+		{
+			return card.costBlack + card.costBlue + card.costGreen + card.costRed + card.costWhite;
+		}
+
+		private double[] CalculateGemValues(IGame state)
+		{
+			double[] values = new double[5];
+
+			for (int m = 0; m < state.Market.Length; m++)
+			{
+				Card card = state.Market[m];
+				int cost = TotalCost(card);
+				for (int color = 0; color < 5; color++)
+				{
+					if (card.Cost((Color)color) > 0)
+					{
+						values[color] += card.value * (1.0 / cost);
+					}
+				}
+			}
+			for (int n = 0; n < state.Nobles.Length; n++)
+			{
+				Noble noble = state.Nobles[n];
+				int cost = noble.requires[0] + noble.requires[1] + noble.requires[2] + noble.requires[3] + noble.requires[4];
+				for (int color = 0; color < 5; color++)
+				{
+					if (noble.requires[color] > 0)
+					{
+						values[color] += noble.value * (1.0 / cost);
+					}
+				}
+			}
+			return values;
+		}
+
+		private double CardRatio(IGame state, IPlayer player, Card card)
+		{
+			int ourDistance = Distance(card, player);
+			int opponentDistance = this.ForBestOpponent(state, p => Distance(card, p));
+			if (ourDistance == 0 && opponentDistance == 0)
+			{
+				return 0.5;
+			}
+			return 1.0 - ((double)ourDistance / (ourDistance + opponentDistance));
+		}
+
+		private int Distance(Card card, IPlayer player)
+		{
+			int distance = 0;
+			distance += Math.Max(0, card.costBlack - player.BuyingPower[(int)Color.Black]);
+			distance += Math.Max(0, card.costGreen - player.BuyingPower[(int)Color.Green]);
+			distance += Math.Max(0, card.costBlue - player.BuyingPower[(int)Color.Blue]);
+			distance += Math.Max(0, card.costRed - player.BuyingPower[(int)Color.Red]);
+			distance += Math.Max(0, card.costWhite - player.BuyingPower[(int)Color.White]);
+			distance -= Math.Max(0, distance - player.Tokens(Color.Gold));
+			return distance;
+		}
+
+		private double NobleRatio(IGame state, IPlayer player, Noble noble)
+		{
+			// ratio of our distance to best opponent's distance
+			int ourDistance = Distance(noble, player);
+			int opponentDistance = this.ForBestOpponent(state, p => Distance(noble, p));
+			if (ourDistance == 0 && opponentDistance == 0)
+			{
+				return 0.5;
+			}
+			return 1.0 - ((double)ourDistance / (ourDistance + opponentDistance));
+		}
+
+		private int ForBestOpponent(IGame state, Func<IPlayer, int> x)
+		{
+			int best = int.MinValue;
+			for (int i = 0; i < state.Players.Length; i++)
+			{
+				if (i == this.PlayerIndex) continue;
+				int value = x(state.Players[i]);
+				if (value > best)
+				{
+					best = value;
+				}
+			}
+			return best;
+		}
+
+		private static int Distance(Noble noble, IPlayer player)
+		{
+			int distance = 0;
+			for (int c = 0; c < 5; c++)
+			{
+				distance += Math.Max(0, noble.requires[c] - player.Gems((Color)c));
+			}
+			return distance;
+		}
+	}
+}
---
+++ Splendor.Model/AI/Choosers/GreedyChooser.cs
@@ -0,0 +1,72 @@
+﻿namespace Splendor.Model.AI
+{
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	public abstract class GreedyChooser : IChooser
+	{
+		private readonly int playerIndex;
+
+		protected int PlayerIndex { get { return this.playerIndex; } }
+
+		public GreedyChooser(int playerIndex)
+		{
+			this.playerIndex = playerIndex;
+		}
+
+		public IAction Choose(IGame state)
+		{
+			var actions = state.AvailableActions.ToArray();
+			if (actions.Length == 0)
+			{
+				return null;
+			}
+			if (actions.Length == 1)
+			{
+				return actions[0];
+			}
+
+			int index = -1;
+			Task<double>[] tasks = new Task<double>[actions.Length];
+			if (state.CurrentPhase == Phase.Choose)
+			{
+				for (int i = 0; i < actions.Length; i++)
+				{
+					IAction action = actions[i];
+					IGame clone = state.Clone();
+					clone.Step(action);
+					while (clone.CurrentPhase != Phase.GameOver && clone.CurrentPhase != Phase.EndTurn)
+					{
+						clone.Step(clone.AvailableActions.FirstOrDefault());
+					}
+					tasks[i] = this.EvaluateStateAsync(clone);
+				}
+				Task.WaitAll(tasks);
+				double max = double.MinValue;
+				double[] results = tasks.Select(t => t.Result).ToArray();
+				for (int i = 0; i < results.Length; i++)
+				{
+					if (results[i] > max)
+					{
+						index = i;
+						max = results[i];
+					}
+				}
+			}
+			if (index == -1)
+			{
+				index = 0;
+			}
+			Debug.WriteLine("Choose {0}", actions[index]);
+			return actions[index];
+		}
+
+		public virtual void PostGame(int winner, IEventSink eventSink, List<IGame>[] history)
+		{
+		}
+
+		protected abstract Task<double> EvaluateStateAsync(IGame state);
+	}
+}
